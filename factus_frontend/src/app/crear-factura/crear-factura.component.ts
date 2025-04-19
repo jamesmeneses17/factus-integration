5c02eb512569c5fac5c959a31fb60442b9b838ea
James Meneses <124942205+jamesmeneses17@users.noreply.github.com>
2025-04-19 00:21:26 -0500

    Aregando las tablas de referencia tipo de documento y metodo de pago

--- factus_frontend/src/app/crear-factura/crear-factura.component.ts
+++ factus_frontend/src/app/crear-factura/crear-factura.component.ts
@@ -7,7 +7,7 @@ import { MunicipiosService } from '../services/municipios.service';
 @Component({
   selector: 'app-crear-factura',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule,],
   templateUrl: './crear-factura.component.html',
 })
 export class CrearFacturaComponent implements OnInit {
@@ -17,7 +17,34 @@ export class CrearFacturaComponent implements OnInit {
   rangoSeleccionado: string = '';
 
   metodoPago: string = '';
-  metodosPago: string[] = ['Efectivo', 'Transferencia', 'Tarjeta'];
+  metodosPago: { codigo: number | string, nombre: string }[] = [
+    { codigo: 10, nombre: 'Efectivo' },
+    { codigo: 42, nombre: 'Consignación' },
+    { codigo: 20, nombre: 'Cheque' },
+    { codigo: 47, nombre: 'Transferencia' },
+    { codigo: 71, nombre: 'Bonos' },
+    { codigo: 72, nombre: 'Vales' },
+    { codigo: 1,  nombre: 'Medio de pago no definido' },
+    { codigo: 49, nombre: 'Tarjeta Débito' },
+    { codigo: 48, nombre: 'Tarjeta Crédito' },
+    { codigo: 'ZZZ', nombre: 'Otro' },
+  ];
+
+  tiposDocumento: { id: number, nombre: string }[] = [
+    { id: 1, nombre: 'Registro civil' },
+    { id: 2, nombre: 'Tarjeta de identidad' },
+    { id: 3, nombre: 'Cédula de ciudadanía' },
+    { id: 4, nombre: 'Tarjeta de extranjería' },
+    { id: 5, nombre: 'Cédula de extranjería' },
+    { id: 6, nombre: 'NIT' },
+    { id: 7, nombre: 'Pasaporte' },
+    { id: 8, nombre: 'Documento de identificación extranjero' },
+    { id: 9, nombre: 'PEP' },
+    { id: 10, nombre: 'NIT otro país' },
+    { id: 11, nombre: 'NUIP' },
+  ];
+  
+  
 
   cliente: any = {
     tipoDocumento: '',
